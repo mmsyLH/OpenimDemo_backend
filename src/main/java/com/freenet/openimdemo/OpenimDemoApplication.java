c6ff3e43f4e99308facc9b6aacadc1d3824fe18d
luohan <luohan@xmfree.net>
2025-02-20 17:52:16 +0800

    初始化

---
+++ src/main/java/com/freenet/openimdemo/OpenimDemoApplication.java
@@ -0,0 +1,13 @@
+package com.freenet.openimdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenimDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenimDemoApplication.class, args);
+    }
+
+}
